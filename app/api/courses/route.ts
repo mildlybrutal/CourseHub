f429fd5cba7e18457e54d70aa78595978e122fc1
mildlybrutal <singuakash23@lpu.in>
2025-08-17 22:20:45 +0530

    feat: :sparkles: fetched all courses

---
+++ app/api/courses/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "../../generated/prisma";
+
+const prisma = new PrismaClient();
+
+export async function GET(req: Request) {
+	const { searchParams } = new URL(req.url);
+	const page = parseInt(searchParams.get("page") || "1");
+	const limit = parseInt(searchParams.get("limit") || "20");
+
+	const skip = (page - 1) * limit;
+
+	try {
+		const courses = await prisma.course.findMany({
+			orderBy: {
+				id: "asc",
+			},
+			skip,
+			take: limit,
+		});
+
+		const totalCourses = await prisma.course.count();
+
+		return NextResponse.json({
+			data: courses,
+			pagination: {
+				total: totalCourses,
+				page,
+				limit,
+				totalPages: Math.ceil(totalCourses / limit),
+			},
+		});
+	} catch (error) {
+		return NextResponse.json(
+			{
+				message: "Failed to fetch courses",
+			},
+			{
+				status: 500,
+			}
+		);
+	}
+}
